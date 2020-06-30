28d6bceec093833cd80251727339ab8a4f5ff3d3
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-06-29 19:17:54 -0600

    refactoring

--- db/test.js
+++ db/test.js
@@ -38,6 +38,12 @@ class DB {
     findAllDepartments() {
         return this.connection.query("SELECT department.id, department.name, SUM (role.salary) AS utilize_budget FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id=department_id GROUP BY department.id, department.department_name")
     }
+    findAllEmployeesByDepartment(department_id) {
+        return this.connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department department ON role.department_id=department.id WHERE department.id= ?", department_id)
+    }
+    findAllEmployeesByManager(manager_id) {
+        return this.connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id WHERE manager_id= ?", manager_id)
+    }
     findAllRoles() {
         return this.connection.query("SELECT role.id, role.title, department.department_name AS department, role.salary FROM role LEFT JOIN department ON role.department_id=department.id")
     }
--- server.js
+++ server.js
@@ -23,33 +23,33 @@ const actions =
 }
 
 // define const that holds all management options
-const viewAllEmployees = [
+const allEmployees = [
     {
         type: 'list',
         message: 'You are viewing all employees.',
     },
 ]
-const viewByDepartment = [
+const byDepartment = [
     {
         type: 'list',
         message: 'You are viewing all employees by department.',
     },
 ]
-const viewByRole = [
+const byRole = [
     {
         type: 'list',
         choices: ['Assistant - 1', 'Team Member - 2', 'Manager - 3', 'Assistant Manager - 4'],
         message: 'You are viewing all employees by role.',
     },
 ]
-const viewByManager = [
+const byManager = [
     {
         type: 'list',
         choices: ['Jackie Brown', 'Clarice Starling', 'Dorothy Gale', 'Scarlett O`Hara'],
         message: 'You are viewing all managers. Please select a manager to view their employees.',
     },
 ]
-const addEmployee = [
+const addNewEmployee = [
     {
         type: 'input',
         message: 'What is the first name of the employee you want to add?',
@@ -89,21 +89,21 @@ const addEmployee = [
         name: 'salary',
     },
 ]
-const addDepartment = [
+const addNewDepartment = [
     {
         type: 'input',
         message: 'What new department would you like to add?',
         name: 'addDepartment',
     },
 ]
-const addRole = [
+const addNewRole = [
     {
         type: 'input',
         message: 'What new role would you like to add?',
         name: 'addRole',
     },
 ]
-const updateDepartment = [
+const updateCurrentEmployee = [
     {
         type: 'list',
         message: 'What would you like to update about this employee?',
@@ -111,23 +111,7 @@ const updateDepartment = [
         name: 'updateEmployee',
     },
 ]
-const updateRole = [
-    {
-        type: 'list',
-        message: 'What would you like to update about this employee?',
-        choices: ['First Name', 'Last Name', 'Manager', 'Role', 'Department',],
-        name: 'updateEmployee',
-    },
-]
-const updateEmployee = [
-    {
-        type: 'list',
-        message: 'What would you like to update about this employee?',
-        choices: ['First Name', 'Last Name', 'Manager', 'Role', 'Department',],
-        name: 'updateEmployee',
-    },
-]
-const updateRole = [
+const updateCurrentRole = [
     {
         type: 'list',
         message: 'What role would you like this employee to have?',
@@ -135,7 +119,7 @@ const updateRole = [
         name: 'updateRole',
     },
 ]
-const updateDepartment = [
+const updateCurrentDepartment = [
     {
         type: 'list',
         message: 'What department would you like this employee to have?',
@@ -143,7 +127,7 @@ const updateDepartment = [
         name: 'updateDepartment',
     },
 ]
-const updateManager = [
+const updateCurrentManager = [
     {
         type: 'list',
         message: 'What manager would you like this employee to have?',
@@ -151,21 +135,21 @@ const updateManager = [
         name: 'updateManager',
     },
 ]
-const deleteEmployee = [
+const deleteThisEmployee = [
     {
         type: 'list',
         message: 'Employee removed from database.',
         name: 'deleteEmployee',
     },
 ]
-const deleteRole = [
+const deleteThisRole = [
     {
         type: 'list',
         message: 'Employee removed from database.',
         name: 'deleteRole',
     },
 ]
-const deleteDepartment = [
+const deleteThisDepartment = [
     {
         type: 'list',
         message: 'Employee removed from database.',
@@ -195,10 +179,10 @@ loadPrompt = async () => {
             return addEmployee()
             break;
         case 'Add Role':
-            return addEmployee()
+            return addRole()
             break;
         case 'Add Department':
-            return addEmployee()
+            return addDepartment()
             break;
         case 'Update Employee':
             return updateEmployee()
@@ -207,7 +191,7 @@ loadPrompt = async () => {
             return updateRole()
             break;
         case 'Update Employee Manager':
-            return updateDept()
+            return updateManager()
             break;
         case 'Remove Employee':
             return deleteEmployee()
@@ -217,35 +201,46 @@ loadPrompt = async () => {
 
 // view by all employees
 async function viewAllEmployees() {
-    return this.allEmployees().then((data) => {
-        return this.renderAllEmployees(data);
-    });
+    const employees = await db.findAllEmployees()
+    console.table(employees)
 }
 
 // view by department
 async function viewByDepartment() {
-    return this.viewByDepartment().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            return sql
-                .search(this.viewByDepartment, [parseInt(arr[0])])
-                .then((data) => {
-                    return this.renderAllEmployees(data);
-                });
-        });
-    });
+    const departments = await db.findAllDepartments()
+    const departmentChoices = departments.map(({ id, name }) => ({
+        name: name, value: id
+    }));
+    const { department_id } = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Which department would you like to view?',
+            name: 'department_id',
+            choices: departmentChoices
+        },
+    ]);
+    const employee = await db.findAllEmployeesByDepartment(department_id)
+    console.table(employee)
+    loadPrompt()
 }
 
 // view by assigned manager
 async function viewByManager() {
-    return this.allManagers().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            return sql.search(this.allManagers, [parseInt(arr[0])]).then((data) => {
-                return this.renderAllEmployees(data);
-            });
-        });
-    });
+    const managers = await db.findAllEmployees()
+    const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`, value: id
+    }));
+    const { manager_id } = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Which manager would you like to view?',
+            name: 'manager_id',
+            choices: managerChoices
+        },
+    ]);
+    const employee = await db.findAllEmployeesByManager(manager_id)
+    console.table(employee)
+    loadPrompt()
 }
 
 // create / add new employee to employee table
@@ -281,132 +276,157 @@ async function addEmployee() {
             message: 'Who is the manager of the employee you want to add?',
             choices: managerChoices,
             name: 'manager_id',
-        }
+        },
         employeeQuestion.manager_id = managerId
     ])
 }
 
 // create / add new role to role table
 async function addRole() {
-    return this.createRole().then((data) => {
-        var inputs = [data.title, data.salary, data.department_id];
-        return sql.insert(this.addRole, inputs).then(function (res) {
-            return res;
-        });
-    });
+    const roles = await db.addRole()
+    const roleChoices = roles.map(({ role_id }) => ({
+        value: role_id
+    }));
+    const { role_id } = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What new role would you like to add?',
+            name: 'role_id',
+            choices: roleChoices
+        },
+    ]);
+    const employee = await db.addRole(role_id)
+    console.table(roles)
+    loadPrompt()
 }
 
 // create / add new department to department table
 async function addDepartment() {
-    return inquirer.prompt([q.department]).then((data) => {
-        return sql
-            .insert(this.addDepartment, [data.department])
-            .then(function (res) {
-                return res;
-            });
-    });
+    const department = await db.addDepartment()
+    const departmentChoices = department.map(({ department_id }) => ({
+        value: department_id
+    }));
+    const { department_id } = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What new department would you like to add?',
+            name: 'department_id',
+            choices: departmentChoices
+        },
+    ]);
+    const employee = await db.addDepartment(department_id)
+    console.table(department)
+    loadPrompt()
 }
 
 // update / make changes to employee table
 async function updateEmployee() {
-    return this.getEmployeeList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            const employeeID = parseInt(arr[0]);
-            return this.createEmployee().then((data) => {
-                var inputs = [
-                    data.first_name,
-                    data.last_name,
-                    data.role_id,
-                    data.manager_id,
-                    data.manager_status,
-                    employeeID,
-                ];
-                const reports = [...data.reports];
-                return sql.update(this.updateEmployee, inputs).then((res) => {
-                    for (let i = 0; i < reports.length; i++) {
-                        sql.update(this.updateEmployeeManager, [empID, reports[i]]);
-                    }
-                });
-            });
-        });
-    });
+
+    // return this.getEmployeeList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         const employeeID = parseInt(arr[0]);
+    //         return this.createEmployee().then((data) => {
+    //             var inputs = [
+    //                 data.first_name,
+    //                 data.last_name,
+    //                 data.role_id,
+    //                 data.manager_id,
+    //                 data.manager_status,
+    //                 employeeID,
+    //             ];
+    //             const reports = [...data.reports];
+    //             return sql.update(this.updateEmployee, inputs).then((res) => {
+    //                 for (let i = 0; i < reports.length; i++) {
+    //                     sql.update(this.updateEmployeeManager, [empID, reports[i]]);
+    //                 }
+    //             });
+    //         });
+    //     });
+    // });
 }
 
 // update / make changes to roles table
 async function updateRole() {
-    return this.getRoleList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            const roleID = parseInt(arr[0]);
-            return this.createRole().then((data) => {
-                var inputs = [data.title, data.salary, data.department_id, roleID];
-                return sql.update(this.updateRoleText, inputs).then((res) => {
-                    return res;
-                });
-            });
-        });
-    });
+
+    // return this.getRoleList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         const roleID = parseInt(arr[0]);
+    //         return this.createRole().then((data) => {
+    //             var inputs = [data.title, data.salary, data.department_id, roleID];
+    //             return sql.update(this.updateRoleText, inputs).then((res) => {
+    //                 return res;
+    //             });
+    //         });
+    //     });
+    // });
 }
 
 // update / make changes to department table
 async function updateDepartment() {
-    return this.getDeptList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            const deptID = parseInt(arr[0]);
-            return this.createDepartment().then((data) => {
-                return sql
-                    .update(this.updateDeptText, [data.department, deptID])
-                    .then((res) => {
-                        return res;
-                    });
-            });
-        });
-    });
+
+    // return this.getDeptList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         const deptID = parseInt(arr[0]);
+    //         return this.createDepartment().then((data) => {
+    //             return sql
+    //                 .update(this.updateDeptText, [data.department, deptID])
+    //                 .then((res) => {
+    //                     return res;
+    //                 });
+    //         });
+    //     });
+    // });
 }
 
 // update assigned manager
-async function updateManager()
+async function updateManager() {
+
+}
 
 // removes selected employee
 async function deleteEmployee() {
-    return this.getEmployeeList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            return sql
-                .delete(this.deleteEmployee, [parseInt(arr[0])])
-                .then((res) => {
-                    return res;
-                });
-        });
-    });
+
+    // return this.getEmployeeList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         return sql
+    //             .delete(this.deleteEmployee, [parseInt(arr[0])])
+    //             .then((res) => {
+    //                 return res;
+    //             });
+    //     });
+    // });
 }
 
 // removes selected role
 async function deleteRole() {
-    return this.getRoleList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            return sql.delete(this.deleteRole, [parseInt(arr[0])]).then((res) => {
-                return res;
-            });
-        });
-    });
+
+    // return this.getRoleList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         return sql.delete(this.deleteRole, [parseInt(arr[0])]).then((res) => {
+    //             return res;
+    //         });
+    //     });
+    // });
 }
 
 // removes selected department
 async function deleteDepartment() {
-    return this.getDepartmentList().then((inquiry) => {
-        return inquirer.prompt([inquiry]).then((data) => {
-            var arr = data.answer.split(" ");
-            return sql
-                .delete(this.deleteDepartment, [parseInt(arr[0])])
-                .then((res) => {
-                    return res;
-                });
-        });
-    });
+
+    // return this.getDepartmentList().then((inquiry) => {
+    //     return inquirer.prompt([inquiry]).then((data) => {
+    //         var arr = data.answer.split(" ");
+    //         return sql
+    //             .delete(this.deleteDepartment, [parseInt(arr[0])])
+    //             .then((res) => {
+    //                 return res;
+    //             });
+    //     });
+    // });
 }
 
 // calling the init function so the app runs
