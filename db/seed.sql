4bbbeb46af1b282bd3b878a3ae314f90210bf55f
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-21 14:24:57 -0600

    seed error

--- db/seed.sql
+++ db/seed.sql
@@ -2,45 +2,26 @@
 USE CMS_db;
 
 --  // define managers so "sort by manager" works
-INSERT INTO employee
-    (id, first_name, last_name, role_id, manager_status)
-VALUES
-    (1, "Jackie", "Brown", 1, TRUE),
-    (2, "Clarice", "Starling", 2, TRUE),
-    (3, "Dorothy", "Gale", 3, TRUE),
-    (4, "Scarlett", "O`Hara", 4, TRUE);
+INSERT INTO department (name)
+VALUES 
+("Sales"), 
+("Finance"), 
+("Engineering"), 
+("Legal");
 
---   // define enough for a good working list
---   // insert random data to display a functioning app
-INSERT INTO employee
-    (id, first_name, last_name, role_id, manager_id, manager_status)
-VALUES
-    (5, "Jake", "Daring", 5, 1, FALSE),
-    (6, "Sach", "Wood", 6, 2, FALSE),
-    (7, "Anika", "Wilde", 7, 2, FALSE),
-    (8, "Holly", "Holy", 8, 1, FALSE),
-    (9, "Layla", "Love", 9, 3, FALSE);
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Sales", 100000, 1), 
+("Accountant", 125000, 2), 
+("Engineering", 150000, 3), 
+("Legal", 250000, 4), 
+("Management", 190000, 5);
 
---   // match to data above and below
-INSERT INTO role
-    (id, title, salary, department_id)
-VALUES
-    (1, "Customer Service Manager", 4000000, 3),
-    (2, "Criminal Manager", 4000000, 3),
-    (3, "Wizard Manager", 4000000, 3),
-    (4, "Mental Health Manager", 4000000, 3),
-    (5, "Customer Service Team Member", 50000, 2),
-    (6, "Criminal Assistant Manager", 1000000, 4),
-    (7, "General Assistant Manager", 1000000, 4),
-    (8, "Customer Service Team Member", 50000, 2),
-    (9, "Assistant to Dorothy Gale", 20000, 1);
-
---   // define enough for a good working list
---   // match to choices defined on server.js
-INSERT INTO department
-    (id, department)
-VALUES
-    (1, "Assistant"),
-    (2, "Team Member"),
-    (3, "Manager"),
-    (4, "Assistant Manager");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+("Pheobe", "Buffet", 4, null), 
+("Joey", "Tribbiani", 2, null), 
+("Rachel", "Green", 3, null), 
+("Ross", "Geller", 1, null), 
+("Monica", "Geller", 1, null), 
+("Chandler", "Bing", 5, null);
